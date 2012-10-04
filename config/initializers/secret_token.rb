38cdd09dac5a3dd0d433a8bce7cd1cf0f9ac8fe8
Kyle Tolle <kyle@nullsix.com>
2012-10-04 08:49:37 -0600

    Created a Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinder::Application.config.secret_token = 'd8af94d0e261a1014b6c5ad3229a92547ad982359654858b0f0912ff269d89ae5256d1768a63857bdf0559650a399e2815eef5679d582a5aebf47200ea020765'
