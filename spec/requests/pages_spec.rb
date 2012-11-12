11cc2c543ea91eab8770a719607572da667c287f
Kyle Tolle <kyle@nullsix.com>
2012-11-12 08:17:02 -0700

    Remove custom page title from home page.
    Remove the custom page title from the home page, but leave it there for
    other pages.

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,10 +3,18 @@ require 'spec_helper'
 describe "Pages" do
 
   describe "Index" do
+    before do
+      visit "pages#index"
+    end
+
+    subject { page }
 
     it "should have the title tinder" do
-      visit "pages#index"
-      page.should have_selector('title', text: 'tinder')
+      should have_selector('title', text: 'tinder')
+    end
+
+    it "should not have a cusom page title" do
+      should_not have_selector('title', text: "|")
     end
   end
 end
