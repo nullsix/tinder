78ae4bb6fa52eb7797a1941da506dd8b0de1715f
Kyle Tolle <kyle@nullsix.com>
2013-01-12 01:51:37 -0700

    Move config vars into env instead of rb constants

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
-OmniAuth.config.full_host = "http://localhost:3000"
+OmniAuth.config.full_host = ENV['FULL_HOST']
 
 # Rails will have loaded config/initializers/google.rb by now.
 # So the constants are available for use.
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, GOOGLE_CLIENT_ID, GOOGLE_API_SECRET
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_API_SECRET']
 end
