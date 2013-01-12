f7ce752ca96a73e1b8fc048721d47f100c626f43
Kyle Tolle <kyle@nullsix.com>
2013-01-11 22:53:05 -0700

    Update to Rails 3.2.11 for security reasons

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tinder::Application.config.secret_token = '2b74d289615bf6413fc56c73d26e8b8a185e00190c28d330c93d6f416536493352ab5b88825f6e8cbb219483251b3359659e83c8ce5e813a119fa0b694c7151e'
+Tinder::Application.config.secret_token = 'fe31f85b3ebe2a20f1408b9056dc566d53b7ba4db0f5e1957e2da85728c90b591762299bfbf6ee1ad4e1d404f6f5c02ea7fbee895a47f2d12c3151b65bd14829'
