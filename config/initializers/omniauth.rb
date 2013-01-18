607f5ca2205d8dda23248cfc926718f2e3e171f0
Kyle Tolle <kyle@nullsix.com>
2013-01-17 21:49:26 -0700

    Update comments in omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.full_host = ENV['FULL_HOST']
 
-# Rails will have loaded config/initializers/google.rb by now.
-# So the constants are available for use.
+# Rails will have loaded config/initializers/env.sh by now.
+# So the environment vars are available for use.
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_API_SECRET']
 end
