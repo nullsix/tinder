fd70b962429531ddf09fd15776b959262bc3ab20
Kyle Tolle <kyle@nullsix.com>
2013-03-21 20:51:07 -0600

    Move application controller methods to sessions helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user, :require_signed_in_user
 
-  private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def signed_in?
-    !!current_user
-  end
-
-  def require_signed_in_user
-    unless signed_in?
-      redirect_to root_url
-    end
-  end
-
-  def owner_is_logged_in?(owning_user)
-    current_user && current_user == owning_user
-  end
+  include SessionsHelper
 end
