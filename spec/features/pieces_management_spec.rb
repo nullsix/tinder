1e124ba0ec5a9a6fce44b7674bd097456ec7010c
Kyle Tolle <kyle@nullsix.com>
2013-04-14 00:28:10 -0600

    Refactor Pieces Mgmt specs and add Pieces#history
    First stab at refactoring Pieces Management specs to be easier to
    read and maintain in the future.
    Add initial specs and view for Pieces#history, based on Versions#index.

--- spec/features/pieces_management_spec.rb
+++ spec/features/pieces_management_spec.rb
@@ -1,145 +1,319 @@
-feature "Pieces Management" do
-  include PieceHelper
-
+shared_context "common" do
   background do
     login_with_oauth
   end
 
   subject { page }
+end
+
+feature "Pieces Management", "User wants to create a new piece" do
+  include_context "common"
 
   context "from the root path" do
     background { visit root_path }
+
     it_behaves_like "a user creating a piece"
   end
 
-  context "when creating pieces with multiple lines" do
-    require 'faker'
-
+  context "from a page with a piece bar" do
     background do
-      visit new_piece_path
+      visit pieces_path
+      first("a.create-piece").click
     end
 
-    [1,3].each do |num|
-      scenario "User sees #{num} lines rendered properly" do
-        expect_create_piece_success "A good title", Faker::Lorem.sentences(num).join("\n\n")
-        within '#content' do
-          should have_css "p", count: num
-        end
+    it_behaves_like "a user creating a piece"
+    #TODO: Does this check to make sure they're redirected to the piece page after creating the piece?
+  end
+end
+
+feature "Pieces Management", "User wants to see text formatted as entered" do
+  include_context "common"
+  require 'faker'
+
+  [1,3].each do |num|
+    scenario "User sees #{num} lines rendered properly" do
+      user = User.last
+      piece = FactoryGirl.create :piece, user: user, versions_count: 0
+      FactoryGirl.create :version,
+        piece: piece,
+        title: "",
+        content: Faker::Lorem.sentences(num).join("\n\n")
+      visit piece_path id: piece.id
+
+      within '#content' do
+        should have_css "p", count: num
       end
     end
   end
+end
 
-  context "from the Pieces path" do
+feature "Pieces Management", "User wants to see all their pieces" do
+  include_context "common"
+
+  context "with no pieces" do
     background { visit pieces_path }
 
     it_behaves_like "piece bar"
 
-    context "with no pieces" do
-      scenario "User sees they have no pieces" do
-        should have_content "your pieces"
-        should have_content "You have no pieces."
+    scenario "User sees they have no pieces" do
+      should have_content "your pieces"
+      should have_content "You have no pieces."
+    end
+  end
+
+  context "with at least one piece" do
+    background do
+      user = User.last
+
+      @pieces = []
+      5.times do |i|
+        @pieces << FactoryGirl.create(:piece, user: user, versions_count: 1)
       end
+      @pieces.reverse!
+
+      visit pieces_path
     end
 
-    context "with many pieces" do
-      before :each do
-        @pieces_count = 5
-        @pieces_count.times do |i|
-          visit new_piece_path
-          expect_create_piece_success i, i
+    #TODO: Should this be in a view spec?
+    it_behaves_like "piece bar"
+
+    scenario "User sees all the pieces displayed" do
+      piece_rows = all "tr.piece-row"
+
+      piece_rows.count.should == @pieces.count
+
+      piece_rows.each.with_index do |piece, i|
+        within piece do
+          within ".piece-links" do
+            should have_link "edit"
+            should have_css ".delete-piece-link"
+            should have_link "all versions"
+          end
+
+          find(".piece-title").text.should == @pieces[i].short_title
+          find(".piece-blurb").text.should == @pieces[i].blurb
+          find(".piece-last-modified").text.should match /Last modified .* ago/
         end
       end
+    end
+  end
+end
 
-      scenario "User sees all the pieces displayed" do
-        visit pieces_path
+shared_context "user piece" do
+  include_context "common" 
 
-        piece_rows = all("tr.piece-row")
+  background do
+    user = User.last
+    @piece = FactoryGirl.create :piece, user: user, versions_count: 1
+  end
+end
 
-        piece_rows.count.should == @pieces_count
+feature "Pieces Management", "User wants to see a piece they've created" do
+  include_context "user piece"
 
-        piece_rows.each_with_index do |piece, index|
-          expected_index = @pieces_count - index - 1
-          within piece do
-            within ".piece-links" do
-              should have_link "edit"
-              should have_css ".delete-piece-link"
-              should have_link "all versions"
+  background { visit pieces_path }
+  
+  scenario "User sees the links for the piece" do
+    should have_link @piece.title
+    has_edit_link
+    has_delete_link
+  end
+
+  context "on the piece page" do
+    background { click_link @piece.title }
+
+    it_behaves_like "piece bar with history" do
+      let(:piece) { @piece }
+    end
+
+    scenario "User can find and view the piece" do
+      should have_content @piece.title
+      should have_content @piece.content
+    end
+  end
+end
+
+feature "Pieces Management", "User wants to edit a piece they've created" do
+  include_context "user piece"
+
+  context "from the pieces page" do
+    background do
+      visit pieces_path
+      first(:link, "edit").click
+    end
+
+    it_behaves_like "a user editing a piece"
+  end
+
+  context "from the piece page" do
+    background do
+      visit piece_path id: @piece.id
+      first(:link, "edit").click
+    end
+
+    it_behaves_like "a user editing a piece"
+  end
+end
+
+feature "Pieces Management", "User wants to delete a piece they've created" do
+  include_context "user piece"
+
+  context "from the pieces page" do
+    background do
+      visit pieces_path
+    end
+
+    it_behaves_like "a user deleting a piece"
+  end
+
+  context "from the piece page" do
+    background do
+      visit piece_path id: @piece.id
+    end
+
+    it_behaves_like "a user deleting a piece"
+  end
+end
+
+feature "Pieces Management", "User views history of a piece they've created" do
+  context "with a user logged in" do
+    context "who is the owner" do
+      include_context "common"
+
+      background do
+        user = User.last
+        @piece = FactoryGirl.create :piece, user: user, versions_count: 5
+
+        @versions = @piece.versions
+        @versions.each.with_index do |v, i|
+          create_draft v if i.even?
+        end
+
+        visit history_piece_path id: @piece.id
+      end
+
+      it_behaves_like "piece bar with history" do
+        let(:piece) { @piece }
+      end
+
+      scenario "User sees all versions and pieces" do
+        version_rows = all(".version-row")
+        version_rows.count.should eq @versions.count
+
+        version_rows.each.with_index do |v, i|
+          expected_version = @versions[i]
+          within v do
+            should have_content expected_version.title
+
+            if expected_version.draft.nil?
+              should have_content "(version ##{expected_version.number})"
+            else
+              should have_content "(draft ##{expected_version.draft.number})"
             end
 
-            find(".piece-title").text.should == expected_index.to_s
-            find(".piece-blurb").text.should == expected_index.to_s
-            find(".piece-last-modified").text.should match /Last modified .* ago/
+            should have_content /Last modified .* ago/
           end
         end
       end
     end
 
-    context "when user clicks on link to create first piece" do
-      background { click_new_link }
+    context "who is not the owner" do
+      include_context "common"
 
-      it_behaves_like "a user creating a piece"
+      background do
+        user = FactoryGirl.create :user, name: "Bob Burger"
+        @piece = FactoryGirl.create :piece, user: user, versions_count: 5
+      end
+
+      context "with no drafts" do
+        background do
+          visit history_piece_path id: @piece.id
+        end
+
+        it_behaves_like "no piece bar"
+
+        scenario "User sees that there are no drafts" do
+          should have_content "This piece has no history you can view."
+        end
+      end
+
+      context "with at least one draft" do
+        background do
+          @drafts = []
+          @piece.versions.each.with_index do |v, i|
+            @drafts << create_draft(v) if i.even?
+          end
+
+          visit history_piece_path id: @piece.id
+        end
+
+        it_behaves_like "no piece bar"
+
+        scenario "User sees the drafts" do
+          version_rows = all(".version-rows")
+
+          version_rows.each.with_index do |v, i|
+            expected_version = @drafts[i]
+
+            within v do
+              should have_content expected_version.title
+
+              should have_content "(draft ##{expect_create_piece_success.draft.number})"
+
+              should have_content /Last modified .* ago/
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context "with no user logged in" do
+    subject { page }
+
+    background do
+      user = FactoryGirl.create :user, name: "Bob Burger"
+      @piece = FactoryGirl.create :piece, user: user, versions_count: 5
     end
 
-    context "when user has created a piece" do
+    context "with no drafts" do
       background do
-        follow_link_and_create_piece
-        @piece = Piece.first
-        @title = @piece.current_version.title
-        @content = @piece.current_version.content
+        visit history_piece_path id: @piece.id
       end
 
-      scenario "sees they have no pieces after deleting the created piece" do
-        gui_delete_piece
-        should have_content "You have no pieces."
+      it_behaves_like "no piece bar"
+
+      scenario "User sees that there are no drafts" do
+        should have_content "This piece has no history you can view."
+      end
+    end
+
+    context "with at least one draft" do
+      background do
+        @drafts = []
+        @piece.versions.each.with_index do |v, i|
+          @drafts << create_draft(v) if i.even?
+        end
+
+        visit history_piece_path id: @piece.id
       end
 
-      context "when on the pieces path" do
-        background do
-          visit pieces_path
+      it_behaves_like "no piece bar"
+
+      scenario "User sees the drafts" do
+        version_rows = all(".version-rows")
+
+        version_rows.each.with_index do |v, i|
+          expected_version = @drafts[i]
+
+          within v do
+            should have_content expected_version.title
+
+            should have_content "(draft ##{expect_create_piece_success.draft.number})"
+
+            should have_content /Last modified .* ago/
+          end
         end
-
-        scenario "User sees links for piece" do
-          should_not have_content "You have no pieces."
-
-          should have_link @title
-
-          has_edit_link
-          has_delete_link
-        end
-
-        scenario "User visits piece and sees content" do
-          click_link @title
-
-          should have_content @title
-          should have_content @content
-        end
-
-        context "when user clicks on the edit link" do
-          background { click_edit_link }
-          
-          it_behaves_like "a user editing a piece"
-        end
-
-        it_behaves_like "a user deleting a piece"
-      end
-      
-      context "and is on a piece page" do
-        background do
-          @piece = Piece.first
-          visit piece_path @piece
-        end
-
-        it_behaves_like "piece bar with history" do
-          let(:piece) { @piece }
-        end
-
-        context "when user clicks on the edit link" do
-          background { click_edit_link }
-          
-          it_behaves_like "a user editing a piece"
-        end
-
-        it_behaves_like "a user deleting a piece"
       end
     end
   end
