2fedee32c663168d1045970cbe8294c3820bf9ac
Kyle Tolle <kyle@nullsix.com>
2013-06-02 23:05:30 -0600

    Remove OmniAuth approval prompt for each login
    If the user has logged into the site before, do not require them to
    acknowledge the approval prompt each time. They will be automatically
    redirected to the site.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,8 @@ OmniAuth.config.full_host = ENV['FULL_HOST']
 # Rails will have loaded config/initializers/env.sh by now.
 # So the environment vars are available for use.
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_API_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_API_SECRET'],
+    {
+      approval_prompt: "auto"
+    }
 end
