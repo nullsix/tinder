072f282b9868de7a5fef3dfb4fca17dc1c05f29a
Kyle Tolle <kyle@nullsix.com>
2013-06-06 09:15:04 -0600

    Issue #25 - Refactor Version validation specs
    Refactor Version validations to be consistent with Piece.
    Move part of #blurb and #short_title specs to be in the instance method
    area.

--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -84,6 +84,38 @@ describe Version do
     end
 
     it { should be_a String }
+
+    context "with a title whose size is > 30" do
+      before :each do
+        @version = build_stubbed :version, title: "a"*31
+      end
+
+      subject { @version }
+
+      specify "has a short_title of 30 characters" do
+        subject.short_title.length.should == 30
+      end
+
+      specify "has first 27 characters of the title" do
+        subject.short_title[0..26].should == "a"*27
+      end
+
+      specify "has '...' as the last 3 characters" do
+        subject.short_title[-3..-1].should == "..."
+      end
+    end
+
+    context "with a title whose size is 30 or less" do
+      before :each do
+        @version = build_stubbed :version, title: "a"*30
+      end
+
+      subject { @version }
+
+      specify "has a short_title which equals the title" do
+        subject.short_title.should == @version.title
+      end
+    end
   end
 
   describe "#content" do
@@ -106,6 +138,38 @@ describe Version do
     end
 
     it { should be_a String }
+
+    context "with a content whose size is > 50" do
+      before :each do
+        @version = build_stubbed :version, content: "a"*51
+      end
+
+      subject { @version }
+
+      specify "has a blurb of 50 characters" do
+        subject.blurb.length.should == 50
+      end
+
+      specify "has first 47 characters of the string" do
+        subject.blurb[0..46].should == "a"*47
+      end
+
+      specify "has '...' as last 3 characters" do
+        subject.blurb[-3..-1].should == "..."
+      end
+    end
+
+    context "with a content whose size is 50 or less" do
+      before :each do
+        @version = build_stubbed :version, content: "a"*50
+      end
+
+      subject { @version }
+
+      specify "has a blurb which equals the content" do
+        subject.blurb.should == @version.content
+      end
+    end
   end
 
   describe "#number" do
@@ -118,161 +182,81 @@ describe Version do
   end
 
   describe "validations" do
-    describe "#title" do
-      it "is valid without a title" do
-        version = build_stubbed :version, title: nil
+    it "is valid without a title" do
+      version = build_stubbed :version, title: nil
 
-        version.should be_valid
-      end
-
-      it "is valid with an empty title" do
-        version = build_stubbed :version, title: ""
-
-        version.should be_valid
-      end
-
-      it "is valid with a title with a single character" do
-        version = build_stubbed :version, title: "a"
-
-        version.should be_valid
-      end
-      
-      it "is valid with a title of 255 characters" do
-        version = build_stubbed :version, title: "a" * 255
-
-        version.should be_valid
-      end
-
-      it "is invalid with a title of more than 255 characters" do
-        version = build_stubbed :version, title: "a" * 256
-
-        version.should_not be_valid
-      end
+      version.should be_valid
     end
 
-    describe "#content" do
-      it "is valid without a content" do
-        version = build_stubbed :version, content: nil
+    it "is valid with an empty title" do
+      version = build_stubbed :version, title: ""
 
-        version.should be_valid
-      end
-
-      it "is valid with an empty content" do
-        version = build_stubbed :version, content: ""
-
-        version.should be_valid
-      end
-
-      it "is valid with a content with a single character" do
-        version = build_stubbed :version, content: "a"
-
-        version.should be_valid
-      end
-      
-      it "is valid with a non-blank content" do
-        should be_valid
-      end
-
-      it "is valid with a content of more than 255 characters" do
-        version = build_stubbed :version, content: "a" * 10_000
-
-        version.should be_valid
-      end
+      version.should be_valid
     end
 
-    describe "#piece" do
-      it "is not valid without a piece" do
-        version = build_stubbed :version, piece: nil
-        version.should_not be_valid
-      end
+    it "is valid with a title with a single character" do
+      version = build_stubbed :version, title: "a"
 
-      it "is valid with a piece" do
-        @version.should be_valid
-      end
+      version.should be_valid
+    end
+    
+    it "is valid with a title of 255 characters" do
+      version = build_stubbed :version, title: "a" * 255
+
+      version.should be_valid
     end
 
-    describe "#blurb" do
-      context "with a content whose size is > 50" do
-        before :each do
-          @version = build_stubbed :version, content: "a"*51
-        end
+    it "is invalid with a title of more than 255 characters" do
+      version = build_stubbed :version, title: "a" * 256
 
-        subject { @version }
-
-        specify "has a blurb of 50 characters" do
-          subject.blurb.length.should == 50
-        end
-
-        specify "has first 47 characters of the string" do
-          subject.blurb[0..46].should == "a"*47
-        end
-
-        specify "has '...' as last 3 characters" do
-          subject.blurb[-3..-1].should == "..."
-        end
-      end
-
-      context "with a content whose size is 50 or less" do
-        before :each do
-          @version = build_stubbed :version, content: "a"*50
-        end
-
-        subject { @version }
-
-        specify "has a blurb which equals the content" do
-          subject.blurb.should == @version.content
-        end
-      end
+      version.should_not be_valid
     end
 
-    describe "#short_title" do
-      context "with a title whose size is > 30" do
-        before :each do
-          @version = build_stubbed :version, title: "a"*31
-        end
+    it "is valid without a content" do
+      version = build_stubbed :version, content: nil
 
-        subject { @version }
-
-        specify "has a short_title of 30 characters" do
-          subject.short_title.length.should == 30
-        end
-
-        specify "has first 27 characters of the title" do
-          subject.short_title[0..26].should == "a"*27
-        end
-
-        specify "has '...' as the last 3 characters" do
-          subject.short_title[-3..-1].should == "..."
-        end
-      end
-
-      context "with a title whose size is 30 or less" do
-        before :each do
-          @version = build_stubbed :version, title: "a"*30
-        end
-
-        subject { @version }
-
-        specify "has a short_title which equals the title" do
-          subject.short_title.should == @version.title
-        end
-      end
+      version.should be_valid
     end
 
-    describe "#number" do
-      context "with a number < 1" do
-        it "is invalid" do
-          version = build_stubbed :version, number: rand(-5..0)
-          version.should_not be_valid
-        end
-      end
+    it "is valid with an empty content" do
+      version = build_stubbed :version, content: ""
 
-      context "with a number >= 1" do
-        it "is valid" do
-          version = build_stubbed :version, number: rand(1..100)
-          version.should be_valid
-        end
-      end
+      version.should be_valid
+    end
+
+    it "is valid with a content with a single character" do
+      version = build_stubbed :version, content: "a"
+
+      version.should be_valid
+    end
+    
+    it "is valid with a non-blank content" do
+      should be_valid
+    end
+
+    it "is valid with a content of more than 255 characters" do
+      version = build_stubbed :version, content: "a" * 10_000
+
+      version.should be_valid
+    end
+
+    it "is not valid without a piece" do
+      version = build_stubbed :version, piece: nil
+      version.should_not be_valid
+    end
+
+    it "is valid with a piece" do
+      version.should be_valid
+    end
+
+    it "is invalid with a number < 1" do
+      version = build_stubbed :version, number: rand(-5..0)
+      version.should_not be_valid
+    end
+
+    it "is valid with a number >= 1" do
+      version = build_stubbed :version, number: rand(1..100)
+      version.should be_valid
     end
   end
 end
