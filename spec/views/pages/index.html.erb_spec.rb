7f97f64750bc436f7faeb87c9528f0328969deea
Kyle Tolle <kyle@nullsix.com>
2013-07-06 20:28:17 -0400

    Issue #51 - Pages index no longer shows piece form
    No longer will a logged in user see a new piece form.
    The main tinder page is shown to everyone. But a logged in user will not
    see the sign in link.

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -3,12 +3,27 @@ require 'spec_helper'
 describe "pages/index" do
   before :each do
     view.stub(:current_user)
-    render
   end
 
   describe "when not logged in" do
+    before :each do
+      view.stub(:signed_in?).and_return(false)
+      render
+    end
+
     it "should show a link to 'Sign in with Google'" do
       rendered.should have_content('Sign in with Google')
+    end
+  end
+
+  describe "when logged in" do
+    before :each do
+      view.stub(:signed_in?).and_return(true)
+      render
+    end
+
+    it "shouldn't show sign in link" do
+      rendered.should_not have_content("Sign in with Google")
     end
   end
 end
