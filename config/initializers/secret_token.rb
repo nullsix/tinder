e986b93cc274120b32667b5c4c5c907ea350d88d
Kyle Tolle <kyle@nullsix.com>
2013-08-11 21:13:46 -0600

    Add secret key base for migration to Rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Tinder::Application.config.secret_token = 'fe31f85b3ebe2a20f1408b9056dc566d53b7ba4db0f5e1957e2da85728c90b591762299bfbf6ee1ad4e1d404f6f5c02ea7fbee895a47f2d12c3151b65bd14829'
+Tinder::Application.config.secret_key_base = 'fe31f85b3ebe2a20f1408b9056dc566d53b7ba4db0f5e1957e2da85728c90b591762299bfbf6ee1ad4e1d404f6f5c02ea7fbee895a47f2d12c3151b65bd14829'
