5f96f05a972816b6ef34bd2952e55186fcfef2f5
Kyle Tolle <kyle@nullsix.com>
2013-08-18 20:03:02 -0600

    Remove trailing whitespace

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,7 +1,7 @@
 class VersionsController < ApplicationController
   respond_to :html
   before_filter :require_signed_in_user, :get_piece
-  
+
   def index
     redirect_to history_piece_path id: @piece.id
   end
@@ -13,7 +13,7 @@ class VersionsController < ApplicationController
   private
   def get_piece
     @piece = current_user.pieces.find params[:piece_id]
-  
+
   # The piece either doesn't exist or doesn't belong to this user.
   rescue ActiveRecord::RecordNotFound
     redirect_to pieces_path
