6e16a2d6b43dfb2d606b7cc5b96e49a36aa4ca5a
Kyle Tolle <kyle@nullsix.com>
2013-09-05 13:14:23 -0600

    Move requires to spork prefork & make test runs faster

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,4 @@
-require 'rubygems'
 require 'spork'
-require 'database_cleaner'
-require 'coveralls'
-Coveralls.wear! 'rails'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
@@ -42,10 +38,14 @@ Spork.prefork do
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
+  require 'rubygems'
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'capybara/rspec'
   require 'capybara/rails'
+  require 'database_cleaner'
+  require 'coveralls'
+  Coveralls.wear! 'rails'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
